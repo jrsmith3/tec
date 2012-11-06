352fd32b3e16672f1b6129c1b59747774000614e
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2012-11-06 15:26:58 -0500

    Refactored.
    Changed from a class to a dictionary.
    Fixed dictionary keys to better comply with PEP8.

--- Constants.py
+++ Constants.py
@@ -1,22 +1,29 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-class Constants:
-  """
-  Defines the various physical constants relevant to the TEC calculations.
+"""
+Defines the various physical constants relevant to the TEC calculations.
   
-  This class defines boltzmann's constant, the electron charge, the
-  electron mass, and the Stefan-Boltzmann constant in SI units.
-  """
-  
-  # Here are the constants in SI units:
-  boltzmann = 1.380657e-23          # [J K^{-1}]
-  permittivity0 = 8.85418781762e-12 # [F m^{-1}]
-  electronCharge = 1.60217738e-19   # [C]
-  electronMass = 9.1093897e-31      # [kg]
-  sigma0 = 5.67050e-8               # [W m^{-2} K^{-4}]
-  
-  # Here are the constants in TEC units
-  #boltzmann = 8.6173423e-5
-  #electronCharge = 1
-  #electronMass = 1
-  #permittivity0 = 8.85418781762e-12 #[F/m] - I'm almost sure these units are wrong.
+This class defines boltzmann's constant, the electron charge, the
+electron mass, and the Stefan-Boltzmann constant in SI units.
+
+SI
+--
+boltzmann = 1.380657e-23          [J K^{-1}]
+permittivity0 = 8.85418781762e-12 [F m^{-1}]
+electron_charge = 1.60217738e-19  [C]
+electron_mass = 9.1093897e-31     [kg]
+sigma0 = 5.67050e-8               [W m^{-2} K^{-4}]
+
+TEC units
+---------
+boltzmann = 8.6173423e-5
+electron_charge = 1
+electron_mass = 1
+permittivity0 = 8.85418781762e-12 #[F/m] - I'm almost sure these units are wrong.
+"""
+
+physical_constants = {boltzmann = 1.380657e-23, \
+                      permittivity0 = 8.85418781762e-12
+                      electron_charge = 1.60217738e-19  
+                      electron_mass = 9.1093897e-31     
+                      sigma0 = 5.67050e-8}
