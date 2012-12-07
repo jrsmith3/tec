4da979e7461203b4ec8f8f7654c3fa08b508a840
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2012-12-07 12:10:35 -0500

    Added tests for Electrode __setitem__ method.

--- test/test_electrode_api.py
+++ test/test_electrode_api.py
@@ -184,7 +184,110 @@ class InstantiationInputOutsideConstraints(unittest.TestCase):
     """Instantiating argument nea < 0."""
     self.input_params["nea"] = -1.0
     self.assertRaises(ValueError,Electrode,self.input_params)
+    
+class SetInputWrongType(unittest.TestCase):
+  """
+  Tests setting attributes when input data is non-numeric.
+  """
+
+  def setUp(self):
+    """
+    Set up a dictionary that can properly instantiate an Electrode object.
+    """
+
+    input_params = {"temp":1,\
+                   "barrier_ht":1,\
+                   "voltage":1,\
+                   "position":0,\
+                   "richardson":10,\
+                   "emissivity":0.5, \
+                   "nea":1.0}
+                   
+    self.El = Electrode(input_params)
+    
+  def test_Electrode_set_temp_non_numeric(self):
+    """Set argument temp non-numeric."""
+    non_num = "this string is non-numeric."
+    self.assertRaises(TypeError,Electrode["temp"],non_num)
+
+  def test_Electrode_set_barrier_ht_non_numeric(self):
+    """Set argument barrier_ht non-numeric."""
+    non_num = "this string is non-numeric."
+    self.assertRaises(TypeError,Electrode["barrier_ht"],non_num)
+
+  def test_Electrode_set_voltage_non_numeric(self):
+    """Set argument voltage non-numeric."""
+    non_num = "this string is non-numeric."
+    self.assertRaises(TypeError,Electrode["voltage"],non_num)
+
+  def test_Electrode_set_position_non_numeric(self):
+    """Set argument position non-numeric."""
+    non_num = "this string is non-numeric."
+    self.assertRaises(TypeError,Electrode["position"],non_num)
+
+  def test_Electrode_set_richardson_non_numeric(self):
+    """Set argument richardson non-numeric."""
+    non_num = "this string is non-numeric."
+    self.assertRaises(TypeError,Electrode["richardson"],non_num)
+
+  def test_Electrode_set_emissivity_non_numeric(self):
+    """Set argument emissivity non-numeric."""
+    non_num = "this string is non-numeric."
+    self.assertRaises(TypeError,Electrode["emissivity"],non_num)
+
+  def test_Electrode_set_nea_non_numeric(self):
+    """Set argument nea non-numeric."""
+    non_num = "this string is non-numeric."
+    self.assertRaises(TypeError,Electrode["nea"],non_num)
+
+
+class SetInputOutsideConstraints(unittest.TestCase):
+  """
+  Tests setting attributes when input values are outside their constraints.
   
+  See the Electrode class docstring for information about the constraints on 
+  the data.
+  """
+  
+  def setUp(self):
+    """
+    Set up a dictionary that can properly instantiate an Electrode object.
+    """
+
+    input_params = {"temp":1,\
+                   "barrier_ht":1,\
+                   "voltage":1,\
+                   "position":0,\
+                   "richardson":10,\
+                   "emissivity":0.5, \
+                   "nea":1.0}
+                   
+    self.El = Electrode(input_params)
+    
+  def test_Electrode_set_temp_less_than_zero(self):
+    """Instantiating argument temp < 0."""
+    self.assertRaises(ValueError,Electrode["temp"],-1.1)
+  
+  def test_Electrode_set_barrier_ht_less_than_zero(self):
+    """Instantiating argument barrier_ht < 0."""
+    self.assertRaises(ValueError,Electrode["barrier_ht"],-1.1)
+  
+  def test_Electrode_set_richardson_less_than_zero(self):
+    """Instantiating argument richardson < 0."""
+    self.assertRaises(ValueError,Electrode["richardson"],-1.1)
+  
+  def test_Electrode_set_emissivity_less_than_zero(self):
+    """Instantiating argument emissivity < 0."""
+    self.assertRaises(ValueError,Electrode["emissivity"],-1.1)
+  
+  def test_Electrode_set_emissivity_greater_than_one(self):
+    """Instantiating argument emissivity > 1."""
+    self.assertRaises(ValueError,Electrode["emissivity"],1.1)
+  
+  def test_Electrode_set_nea_less_than_zero(self):
+    """Instantiating argument nea < 0."""
+    self.assertRaises(ValueError,Electrode["nea"],-1.1)
+
 
 if __name__ == '__main__':
   unittest.main()
