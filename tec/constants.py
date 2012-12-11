3af6451592cc6d3c1d1c769db0a29437cbf7b3a0
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2012-12-11 11:52:52 -0500

    Fixed values  of Boltzmann's constant. Others still need to be checked.

--- tec/constants.py
+++ tec/constants.py
@@ -16,7 +16,7 @@ sigma0 = 5.67050e-8               [W m^{-2} K^{-4}]
 
 TEC units
 ---------
-boltzmann = 8.6173423e-5          [eV K^{-1}]
+boltzmann = 8.6173324e-5          [eV K^{-1}]
 electron_charge = 1
 electron_mass = 1
 permittivity0 = 8.85418781762e-12 [F/m] - I'm almost sure these units are wrong.
@@ -28,7 +28,7 @@ __date__ = ""
 __copyright__ = "Copyright (c) 2012 Joshua Ryan Smith"
 __license__ = ""
 
-physical_constants = {"boltzmann" : 1.380657e-23, \
+physical_constants = {"boltzmann" : 1.3806488e-23, \
                       "permittivity0" : 8.85418781762e-12, \
                       "electron_charge" : 1.60217738e-19, \
                       "electron_mass" : 9.1093897e-31, \
