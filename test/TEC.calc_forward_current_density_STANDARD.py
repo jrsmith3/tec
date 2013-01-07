f60106ad7fa2c76e604a2e7bc238a6c0b19d124a
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-01-07 13:39:20 -0500

    Refactored so data better matches attributes of TEC class. Removed references to ipython notebook.
    This script has now evolved to the point where it isn't similar to what was originially done in the ipython notebook.

--- test/TEC.calc_forward_current_density_STANDARD.py
+++ test/TEC.calc_forward_current_density_STANDARD.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
-# <nbformat>2</nbformat>
 
 """
 Generates the standard data for the TEC calc_forward_current_density method.
-
-This script was generated from an ipython notebook session.
 """
-
-# <codecell>
 
 import numpy as np
 import pickle
-
-# <codecell>
+import copy
 
 def calc_special_case(params):
     """
@@ -24,19 +18,19 @@ def calc_special_case(params):
     boltzmann = 8.6173324e-5
     
     # Ignore the negative space charge effect and determine the maximum motive.    
-    if (params["em_barrier_ht"] + params["em_voltage"]) > \
-      (params["co_barrier_ht"] + params["co_voltage"]):
-        psi_m = params["em_barrier_ht"]
+    if (params["Emitter"]["barrier_ht"] + params["Emitter"]["voltage"]) > \
+      (params["Collector"]["barrier_ht"] + params["Collector"]["voltage"]):
+        psi_m = params["Emitter"]["barrier_ht"]
     else:
-        psi_m = params["co_barrier_ht"] + \
-          (params["co_voltage"] - params["em_voltage"])
+        psi_m = params["Collector"]["barrier_ht"] + \
+          (params["Collector"]["voltage"] - params["Emitter"]["voltage"])
     
     # Adjust the emitter temperature to yield an argument for the exponent which is a natural number.
-    n = np.round(psi_m / (boltzmann * params["em_temp"]))
-    params["em_temp"] = psi_m / (boltzmann * n)
+    n = np.round(psi_m / (boltzmann * params["Emitter"]["temp"]))
+    params["Emitter"]["temp"] = psi_m / (boltzmann * n)
     
     # Adjust the emitter Richardson's constant to yield a value of current which is a natrual number.
-    forward_current_density = (params["em_richardson"] * psi_m**2) / \
+    forward_current_density = (params["Emitter"]["richardson"] * psi_m**2) / \
       (np.exp(n) * n**2 * boltzmann**2)
         
     # Break off the exponent, round the resulting mantissa, and put everything back together. If the forward current density is 0, the following calculation will yield a value of NaN for the Richardson's constant, so just leave Richardson alone.
@@ -44,16 +38,14 @@ def calc_special_case(params):
         exponent = np.floor(np.log10(forward_current_density))
         forward_current_density = np.round(forward_current_density / 10**exponent) * 10**exponent
         
-        params["em_richardson"] = (forward_current_density * np.exp(n) * n**2 * boltzmann**2) / \
+        params["Emitter"]["richardson"] = (forward_current_density * np.exp(n) * n**2 * boltzmann**2) / \
           psi_m**2
     
     params["forward_current_density"] = forward_current_density
-    params["n"] = n
+    params["Emitter"]["n"] = n
     params["psi_m"] = psi_m
     
     return params
-
-# <codecell>
 
 def print_results(params):
     """
@@ -63,64 +55,54 @@ def print_results(params):
     boltzmann = 8.6173324e-5
 
     print "n = psi_m/(boltzmann * em_temp)"
-    print str(params["n"]) + " = " + str(params["psi_m"]) + "/(" + \
-          str(boltzmann) + " * " + str(params["em_temp"]) + ")"
+    print str(params["Emitter"]["n"]) + " = " + str(params["psi_m"]) + "/(" + \
+          str(boltzmann) + " * " + str(params["Emitter"]["temp"]) + ")"
     print "\r"
     print "forward_current_density = (em_richardson * psi_m**2)/" +\
           "\r  (exp(n) * n**2 * boltzmann**2)"
     print str(params["forward_current_density"]) + \
-          " = (" + str(params["em_richardson"]) + " * " +\
+          " = (" + str(params["Emitter"]["richardson"]) + " * " +\
           str(params["psi_m"]) + "**2)/ \\" + \
-          "\r  (np.exp(" + str(params["n"]) + ") * " + str(params["n"]) + \
+          "\r  (np.exp(" + str(params["Emitter"]["n"]) + ") * " + str(params["Emitter"]["n"]) + \
           "**2 * " + str(boltzmann) + "**2)"
     print "\r"
         
     print "Emitter"
-    print " richardson:", params["em_richardson"]
-    print " barrier_ht:", params["em_barrier_ht"]
-    print " temp:      ", params["em_temp"]
-    print " voltage:   ", params["em_voltage"]
+    print " richardson:", params["Emitter"]["richardson"]
+    print " barrier_ht:", params["Emitter"]["barrier_ht"]
+    print " temp:      ", params["Emitter"]["temp"]
+    print " voltage:   ", params["Emitter"]["voltage"]
     print "Collector"
-    print " barrier_ht:", params["co_barrier_ht"]
-    print " voltage:   ", params["co_voltage"]
+    print " barrier_ht:", params["Collector"]["barrier_ht"]
+    print " voltage:   ", params["Collector"]["voltage"]
     print "\r"
     print "forward_current_density:", params["forward_current_density"]
     print "========================================"
     
-# <codecell>
-
-em_richardsons = [0.01,100]
-em_barrier_hts = [0.5,5.0]
-em_temps = [200,2000]
-em_voltages = [-10,10]
-
-co_barrier_hts = [0.5,5.0]
-co_voltages = [-10,10]
+richardsons = [0.01,100]
+barrier_hts = [0.5,5.0]
+temps = [200,2000]
+voltages = [-10,10]
 
 std = []
 
-# <codecell>
-
-for em_richardson in em_richardsons:
-    for em_barrier_ht in em_barrier_hts:
-        for em_temp in em_temps:
-            for em_voltage in em_voltages:
-                for co_barrier_ht in co_barrier_hts:
-                    for co_voltage in co_voltages:
-                        params = {"em_richardson":em_richardson,
-                                  "em_barrier_ht":em_barrier_ht,
-                                  "em_temp":em_temp,
-                                  "em_voltage":em_voltage,
-                                  "co_barrier_ht":co_barrier_ht,
-                                  "co_voltage":co_voltage}
-                        params = calc_special_case(params)
-                        std.append(params)                        
-
-# <codecell>
+for em_richardson in richardsons:
+    for em_barrier_ht in barrier_hts:
+        for em_temp in temps:
+            for em_voltage in voltages:
+                for co_barrier_ht in barrier_hts:
+                    for co_voltage in voltages:
+                      em = {"richardson": em_richardson,\
+                            "temp": em_temp,\
+                            "barrier_ht": em_barrier_ht,\
+                            "voltage": em_voltage}
+                      co = {"barrier_ht": co_barrier_ht,\
+                            "voltage": co_voltage}
+                      params = {"Emitter": copy.deepcopy(em), "Collector": copy.deepcopy(co)}
+                      calc_special_case(params)
+                      std.append(copy.deepcopy(params))
 
 pickle.dump(std,open("TEC.calc_forward_current_density_STANDARD.dat","w"))
-
-# <codecell>
 
 #srt = sorted(std, key=lambda itm: itm["forward_current_density"])
 #for itm in srt:
