c2b06ca5be551de7eaed7e3e41c980abfb7aedf3
Joshua R Smith <joshua.r.smith@gmail.com>
2013-01-07 11:01:51 -0500

    Drafted code to fill out the data in nonsymhfcomp.

--- test/TEC.calc_output_current_density_STANDARD.py
+++ test/TEC.calc_output_current_density_STANDARD.py
@@ -28,8 +28,56 @@ def complement(tecparams):
 		outparams["output_current_density"] = -1 * tecparams["output_current_density"]
 
 	return outparams
-
 	
+def compare_params(tecparams, fcdparams):
+  """
+  Compare a set of tecparams against forward current density standard data.
+  
+  This method returns a value of True or False. It first method checks the voltage and barrier_ht attributes and ensures they are equal for both electrodes across the two sets of params. Only one of the electrodes in fcdparams will have richardson and temp attributes; this method finds which electrode has them, and compares them against the attributes of the corresponding electrode in tecparams. This comparison is fuzzy; the values have to match within a factor of two to be considered equal.
+  """
+  iseq = True
+  eqparams = ["barrier_ht", "voltage"]
+  for el in tecparams.keys():
+    for eqparam in eqparams:
+      iseq = iseq * (tecparams[el][eqparam] == fcdparams[el][eqparam])
+      
+  # Figure out which electrode in fcdparams has temp and richardson.
+  full_el = find_full_params(fcdparams)
+      
+  almost_eqparams = ["temp", "richardson"]
+  for almost_eqparam in almost_eqparams:
+    if (tecparams[full_el][almost_eqparam]/fcdparams[full_el][almost_eqparam] < 2) && \
+       (tecparams[full_el][almost_eqparam]/fcdparams[full_el][almost_eqparam] > 0.5):
+	 iseq = iseq * True
+    else:
+      iseq = iseq * False
+      
+  return iseq
+  
+def find_full_params(fcdparams):
+  """
+  Returns the name of the electrode with the full set of parameters.
+  
+  The forward and back current density standard data consists of a dictionary with two fields, "Emitter" and "Collector", corresponding to the emitter and collector electrode. One of these electrodes does not have the full set of parameters that a typical Electrode object would have. This method returns the name of the electrode that has the full set of parameters an Electrode object should have.
+  """
+  for el in fcdparams.keys():
+    if "temp" in fcdparams[el]:
+      return el
+	
+
+def merge_params(tecparams, fcdparams):
+  """
+  Merge a set of forward/back current standard data into a set of tecparams.
+  
+  This method determines which electrode of forward/back current density standard data is completely filled out; i.e. has attributes for richardson, temp, and current_density. It then overwrites the corresponding electrode in tecparams with this data.
+  
+  Please note that this method does no comparisons between tecparams and fcdparams, it just clobbers whatever data is in tecparams.
+  """
+  full_el = find_full_params(fcdparams)
+  
+  tecparams[full_el] = copy.deepcopy(fcdparams[full_el])
+
+#fcdstd = pickle.load("TEC.calc_forward_current_density_STANDARD.dat")
 
 richardsons = [0.01, 100]
 temps = [200, 2000]
@@ -73,6 +121,14 @@ for tecparams in sym:
 	
 # Perturb symmetric cases
 
+# Fill out data for each set of params in nonsymhfcomp using data from the forward current density standard data.
+for tecparams in nonsymhfcomp:
+  for fcdparams in fcdstd:
+    if compare_params(tecparams, fcdparams):
+      merge_params(tecparams, fcdparams)
+    elif compare_params(tecparams, complement(fcdparams)):
+      merge_params(tecparams, complement(fcdparams))
+
 # Sort nonsymhfcomp into trivial and not obviously trivial.
 nonsymhfcomptrivial = []
 nonsymhfcompnontrivial = []
@@ -83,10 +139,5 @@ for tecparams in nonsymhfcomp:
 		nonsymhfcompnontrivial.append(copy.deepcopy(tecparams))
 
 # Deal with nonsymhfcomptrivial
-eqparams = ["barrier_ht", "voltage"]
-for el in tecparams.keys():
-    print "==", el, "=="
-    for eqparam in eqparams:
-        print eqparam, ":", tecparams[el][eqparam] == fcdparams[el][eqparam]
 
 # Deal with nonsymhfcompnontrivial
