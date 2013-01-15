cc129cf74cb429f8a82ee34a6141cbd110d6929e
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-01-15 14:17:06 -0500

    Cosmetic changes to class.

--- tec/dimensionlesslangmuirpoissonsoln.py
+++ tec/dimensionlesslangmuirpoissonsoln.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from scipy import interpolate,integrate,special,optimize
+from scipy import interpolate,integrate,special
 import numpy as np
 
-class DimensionlessLangmuirPoissonSoln():
+class DimensionlessLangmuirPoissonSoln(dict):
   """
   Approximation of solution of Langmuir's dimensionless Poisson's equation.
   """
@@ -17,8 +17,8 @@ class DimensionlessLangmuirPoissonSoln():
     # 4. Solve both the lhs and rhs odes.
     # 5. Create the lhs and rhs interpolation objects.
     
-    self["lhs"] = calc_branch(-2.5538)
-    self["rhs"] = calc_branch(100)
+    self["lhs"] = self.calc_branch(-2.5538)
+    self["rhs"] = self.calc_branch(100)
       
   def calc_branch(self, endpoint, num_points = 1000):
     """
@@ -27,10 +27,13 @@ class DimensionlessLangmuirPoissonSoln():
     ics = np.array([0,0])
     position_array = np.linspace(0, endpoint, num_points)
     motive_array = integrate.odeint(self.langmuir_poisson_eq,ics,position_array)
+    
+    # NOTE: the InterpolatedUnivariateSpline class requires that the abscissae are monotonically increasing. That is not necessarily the case here.
+    # FWIW, here's an easy way to flip a numpy array around: ar[::-1]
     motive_v_position = \
-      interpolate.InterpolatedUnivariateSpline(position_array,motive_array)
+      interpolate.InterpolatedUnivariateSpline(position_array,motive_array[:,0])
     position_v_motive = \
-      interpolate.InterpolatedUnivariateSpline(motive_array,position_array)
+      interpolate.InterpolatedUnivariateSpline(motive_array[:,0],position_array)
       
     return {"motive_v_position": motive_v_position, \
             "position_v_motive": position_v_motive}
@@ -48,7 +51,7 @@ class DimensionlessLangmuirPoissonSoln():
     """
     Return motive given a value of position.
     """
-    if position > 0:
+    if position >= 0:
       return self["rhs"]["motive_v_position"](position)
     else:
       return self["lhs"]["motive_v_position"](position)
