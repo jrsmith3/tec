b39eed8f974fa6dab67d600e5d53118a57df8b66
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-01-17 14:44:39 -0500

    Roughed out the motive calculator and the output voltage target function.

--- tec/tec_langmuir.py
+++ tec/tec_langmuir.py
@@ -51,7 +51,7 @@ class TEC_Langmuir(TEC):
     # For brevity, "dimensionless" prefix omitted from "position" and "motive" variable names.
     
     # Rootfinder to get critical point output current density.
-    output_current_density = 0
+    output_current_density = 0.0
     
     position = -self.calc_interelectrode_spacing() * \
       ((2 * np.pi * physical_constants["electron_mass"] * physical_constants["electron_charge"]**2) / \
@@ -71,6 +71,21 @@ class TEC_Langmuir(TEC):
        "dimensionless_motive":motive,
        "output_voltage":output_voltage,
        "output_current_density":output_current_density}
+       
+  def calc_motive(self):
+    """
+    Calculate the motive parameters.
+    
+    I'm not really sure where this method is going to go or if it will keep this name, but I know I will need all this code somewhere.
+    """
+    # For brevity, "dimensionless" prefix omitted from "position" and "motive" variable names.
+    
+    # Rootfinder to get output current density corresponding to output voltage.
+    output_current_density = 0.0
+    
+    em_motive = np.log(self["Emitter"].calc_output_current()/output_current_density)
+    
+    # NOTE: At this point I pretty much have what I need. I can get the other dimensionless parameters simply by knowing the value of output current. I can get the emitter and collector dimensionless motives and positions.
   
   def critical_point_target_function(self,output_current_density):
     """
@@ -88,3 +103,20 @@ class TEC_Langmuir(TEC):
     
     return position - self["dps"].get_position(motive)
 
+  def output_voltage_target_function(self,output_current_density):
+    """
+    Target function for the output voltage rootfinder.
+    """
+    # For brevity, "dimensionless" prefix omitted from "position" and "motive" variable names.
+    em_motive = np.log(self["Emitter"].calc_saturation_current/output_current_density)
+    em_position = self["motive_data"]["dps"].get_position(em_motive)
+    
+    # NOTE: I haven't yet defined x0. Probably it will end up as a helper method.
+    co_position = self.calc_interelectrode_spacing()/x0 + em_position
+    co_motive = self["motive_data"]["dps"].get_motive(co_position)
+    
+    return ((self["Emitter"]["barrier_ht"] + \
+      em_motive * physical_constants["boltzmann"] * self["Emitter"]["temperature"]) - \
+      (self["Collector"]["barrier_ht"] + \
+      co_motive * physical_constants["boltzmann"] * self["Emitter"]["temperature"]))/ \
+      physical_constants["electron_charge"]
