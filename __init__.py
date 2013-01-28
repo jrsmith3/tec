72f77b49a199c3192b1333bacde921acf315822f
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-01-28 10:43:04 -0500

    Added include stmt for TEC_Langmuir.

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@
 from electrode import Electrode
 from constants import physical_constants
 from tec import TEC
+from tec_langmuir import TEC_Langmuir
 from dimensionlesslangmuirpoissonsoln import DimensionlessLangmuirPoissonSoln
