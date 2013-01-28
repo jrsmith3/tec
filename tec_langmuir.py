a588fd8701f6b2b10d41f2382b8227a6fe143ce2
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-01-28 08:49:02 -0500

    Rewrote TEC_Langmuir docstring.
    The docstring is formatted more like that of the TEC class and is also much more descriptive.

--- tec_langmuir.py
+++ tec_langmuir.py
@@ -9,9 +9,79 @@ from tec import TEC
 
 class TEC_Langmuir(TEC):
   """
-  Implementation of Langmuir's TEC model.
-  """
-  
+  Thermionic engine simulator. Considers space charge, ignores NEA.
+
+  dict-like object that implements a model of electron transport including the negative space charge effect. This class explicitly ignores the fact that either electrode may have NEA and determines the vacuum level of an electrode at the barrier. The model is based on [1].
+
+  Attributes
+  ----------
+  The attributes of the object are accessed like a dictionary. The object has three attributes; "Emitter" and "Collector" are both Electrode objects. "motive_data" is a dictionary containing (meta)data calculated during the motive calculation. "motive_data" should usually be accessed via the class's convenience methods. "motive_data" contains the following data:
+
+    motive_array:   A two-element array containing the electrostatic boundary 
+                    conditions, i.e. the vacuum level of the emitter and 
+                    collector, respectively.
+    
+    position_array: A two-element array containing the values of position 
+                    corresponding to the values in motive_array.
+                    
+    saturation_pt:  Dict containing saturation point data described below. Only 
+                    contains dimensionless quantities at the collector since the
+                    emitter dimensionless quantities are all zero by definition.
+    
+      voltage:      Voltage at which the saturation point occurs.
+      
+      current:      Current at which the saturation point occurs.
+      
+      co_motive:    Dimensionless motive at the collector.
+      
+      co_position:  Dimensionless position at the collector.
+    
+    critical_pt:    Dict containing critical point data described below. Only 
+                    contains dimensionless quantities at the emitter since the
+                    collector dimensionless quantities are all zero by 
+                    definition.
+    
+      voltage:      Voltage at which the critical point occurs.
+      
+      current:      Current at which the critical point occurs.
+      
+      em_motive:    Dimensionless motive at the emitter.
+      
+      em_position:  Dimensionless position at the emitter.
+
+                    
+    motive_interp:  A scipy.interpolate.interp1d object that interpolates the 
+                    two arrays described above used in the class's convenience 
+                    methods.
+
+  Parameters
+  ----------
+  The TEC_Langmuir class is instantiated by a dict with two keys, "Emitter" and "Collector" (case insensitive). Both keys have data that is also of type dict which are configured to instantiate an Electrode object. Additional keys will be ignored and there are no default values for instantiation.
+
+  Examples
+  --------
+  >>> em_dict = {"temp":1000,
+  ...            "barrier_ht":1,
+  ...            "voltage":0,
+  ...            "position":0,
+  ...            "richardson":10,
+  ...            "emissivity":0.5}
+  >>> co_dict = {"temp":300,
+  ...            "barrier_ht":0.8,
+  ...            "voltage":0,
+  ...            "position":10,
+  ...            "richardson":10,
+  ...            "emissivity":0.5}
+  >>> input_dict = {"Emitter":em_dict, "Collector":co_dict}
+  >>> example_tec = TEC(input_dict)
+
+  Notes
+  -----
+
+  Bibliography
+  ------------
+  [1] Langmuir
+  """  
   def calc_back_current_density(self):
     """
     Return back current density in A m^{-2}.
