ff7a57bad09471354012e478894de5c5b8ff93c7
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-02-11 10:48:35 -0500

    Import TEC_NEAC.

--- __init__.py
+++ __init__.py
@@ -4,4 +4,5 @@ from electrode import Electrode
 from constants import physical_constants
 from tec import TEC
 from tec_langmuir import TEC_Langmuir
+from tec_neac import TEC_NEAC
 from dimensionlesslangmuirpoissonsoln import DimensionlessLangmuirPoissonSoln
