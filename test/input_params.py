2e398bfe1e8b600798e7ecec05055be8db2a2b2a
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-02-26 15:31:42 -0500

    Container of typical input parameters for tests.
    This module (or something like it) seemed necessary since I was repeating a bunch of code in the setUp methods of these tests.

---
+++ test/input_params.py
@@ -0,0 +1,11 @@
+"""
+Module containing common input parameters for tests.
+"""
+
+electrode_input_params = {"temp":1,\
+			              "barrier":1,\
+			              "voltage":1,\
+			              "position":0,\
+			              "richardson":10,\
+			              "emissivity":0.5,\
+			              "nea":1.0}
