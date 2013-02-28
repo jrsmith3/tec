8874c3b711d35a764be71b250ab9a19b5b2bdcfc
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-02-27 21:37:58 -0500

    Works with renamed TECBase. Removed cruft.

--- test/test_tec_api.py
+++ test/test_tec_api.py
@@ -1,23 +1,16 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 """
-Tests API of the TEC class.
+Tests API of the TECBase class.
 
 I will eventually need to fix this note, but here it is. I am intentionally not
 testing the cases where the sub-dicts have incorrect data. The Electrode tests
 deal with these cases comprehensively and there is no additional logic at the 
-TEC level. In the event that I do need to test these cases, I can always reach
+TECBase level. In the event that I do need to test these cases, I can always reach
 back in the VCS history and grab the previous version of this file.
 """
 
-__author__ = "Joshua Ryan Smith (joshua.r.smith@gmail.com)"
-__version__ = ""
-__date__ = ""
-__copyright__ = "Copyright (c) 2012 Joshua Ryan Smith"
-__license__ = ""
-
-from tec import TEC
+from tec import TECBase
 import unittest
 
 class InstantiationInputNonDict(unittest.TestCase):
@@ -25,13 +18,13 @@ class InstantiationInputNonDict(unittest.TestCase):
   Tests instantiation when non-dict data is used.
   """
 
-  def test_TEC_no_input_arg(self):
-    """Attempt to instantiate TEC with no input argument."""
-    self.assertRaises(TypeError,TEC,None)
+  def test_TECBase_no_input_arg(self):
+    """Attempt to instantiate TECBase with no input argument."""
+    self.assertRaises(TypeError,TECBase,None)
   
-  def test_TEC_non_dict_input_arg(self):
+  def test_TECBase_non_dict_input_arg(self):
     """Attempt to instantiate Electrode with a non-dict input argument."""
-    self.assertRaises(TypeError,TEC,"this string is not a dict.")
+    self.assertRaises(TypeError,TECBase,"this string is not a dict.")
 
 
 class InstantiationInputIncomplete(unittest.TestCase):
@@ -41,7 +34,7 @@ class InstantiationInputIncomplete(unittest.TestCase):
   
   def setUp(self):
     """
-    Set up a dictionary that can properly instantiate a TEC object.
+    Set up a dictionary that can properly instantiate a TECBase object.
     """
 
     em_params = {"temp":1,\
@@ -62,15 +55,15 @@ class InstantiationInputIncomplete(unittest.TestCase):
                    
     self.input_params = input_params
     
-  def test_TEC_input_arg_sans_Emitter(self):
+  def test_TECBase_input_arg_sans_Emitter(self):
     """Instantiating argument missing Emitter."""
     del(self.input_params["Emitter"])
-    self.assertRaises(KeyError,TEC,self.input_params)
+    self.assertRaises(KeyError,TECBase,self.input_params)
   
-  def test_TEC_input_arg_sans_Collector(self):
+  def test_TECBase_input_arg_sans_Collector(self):
     """Instantiating argument missing Emitter."""
     del(self.input_params["Collector"])
-    self.assertRaises(KeyError,TEC,self.input_params)  
+    self.assertRaises(KeyError,TECBase,self.input_params)  
   
 class InstantiationInputFieldsWrongType(unittest.TestCase):
   """
@@ -79,7 +72,7 @@ class InstantiationInputFieldsWrongType(unittest.TestCase):
 
   def setUp(self):
     """
-    Set up a dictionary that can properly instantiate a TEC object.
+    Set up a dictionary that can properly instantiate a TECBase object.
     """
 
     em_params = {"temp":1,\
@@ -100,15 +93,15 @@ class InstantiationInputFieldsWrongType(unittest.TestCase):
                    
     self.input_params = input_params
 
-  def test_TEC_input_Emitter_non_numeric(self):
+  def test_TECBase_input_Emitter_non_numeric(self):
     """Instantiating argument Emitter is non-numeric."""
     self.input_params["Emitter"] = "this string is non-numeric."
-    self.assertRaises(TypeError,TEC,self.input_params)
+    self.assertRaises(TypeError,TECBase,self.input_params)
   
-  def test_TEC_input_Collector_non_numeric(self):
+  def test_TECBase_input_Collector_non_numeric(self):
     """Instantiating argument Collector is non-numeric."""
     self.input_params["Collector"] = "this string is non-numeric."
-    self.assertRaises(TypeError,TEC,self.input_params)
+    self.assertRaises(TypeError,TECBase,self.input_params)
   
 
 class SetInputWrongType(unittest.TestCase):
@@ -118,7 +111,7 @@ class SetInputWrongType(unittest.TestCase):
 
   def setUp(self):
     """
-    Set up a TEC object for the tests
+    Set up a TECBase object for the tests
     """
 
     em_params = {"temp":1,\
@@ -137,26 +130,26 @@ class SetInputWrongType(unittest.TestCase):
                  
     input_params = {"Emitter":em_params, "Collector":co_params}
                    
-    self.TEC_obj = TEC(input_params)
+    self.TECBase_obj = TECBase(input_params)
     
-  def test_TEC_set_Emitter_non_numeric(self):
+  def test_TECBase_set_Emitter_non_numeric(self):
     """Set argument Emitter non-numeric."""
     non_num = "this string is non-numeric."
-    self.assertRaises(TypeError,self.TEC_obj["Emitter"],non_num)
+    self.assertRaises(TypeError,self.TECBase_obj["Emitter"],non_num)
 
-  def test_TEC_set_Collector_non_numeric(self):
+  def test_TECBase_set_Collector_non_numeric(self):
     """Set argument Collector non-numeric."""
     non_num = "this string is non-numeric."
-    self.assertRaises(TypeError,self.TEC_obj["Collector"],non_num)
+    self.assertRaises(TypeError,self.TECBase_obj["Collector"],non_num)
     
 class CalculatorsReturnType(unittest.TestCase):
   """
-  Tests output types of the TEC calculator methods.
+  Tests output types of the TECBase calculator methods.
   """
 
   def setUp(self):
     """
-    Set up a TEC object for the tests
+    Set up a TECBase object for the tests
     """
 
     em_params = {"temp":1,\
@@ -175,13 +168,13 @@ class CalculatorsReturnType(unittest.TestCase):
                  
     input_params = {"Emitter":em_params, "Collector":co_params}
                    
-    self.TEC_obj = TEC(input_params)
+    self.TECBase_obj = TECBase(input_params)
     
   #def test_get_motive_numeric(self):
     #"""
     #get_motive returns numeric type.
     #"""
-    #self.assertTrue(isinstance(self.TEC_obj.get_motive(0.5),float))
+    #self.assertTrue(isinstance(self.TECBase_obj.get_motive(0.5),float))
   
   #def test_get_motive_numpy_array(self):
     #"""
@@ -193,92 +186,89 @@ class CalculatorsReturnType(unittest.TestCase):
     """
     get_max_motive_ht returns numeric type.
     """
-    self.assertTrue(isinstance(self.TEC_obj.get_max_motive_ht(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.get_max_motive_ht(),float))
   
   def test_get_max_motive_ht_with_position(self):
     """
     get_max_motive_ht returns tuple.
     """
-    self.assertTrue(isinstance(self.TEC_obj.get_max_motive_ht(with_position=1),tuple))
+    self.assertTrue(isinstance(self.TECBase_obj.get_max_motive_ht(with_position=1),tuple))
   
   def test_get_max_motive_ht_with_position_tuple_items(self):
     """
     get_max_motive_ht returns tuple with numeric items.
     """
-    tup = self.TEC_obj.get_max_motive_ht(with_position=1)
+    tup = self.TECBase_obj.get_max_motive_ht(with_position=1)
     self.assertTrue(all( isinstance(indx,float) for indx in tup ))
   
   def test_calc_interelectrode_spacing(self):
     """
     calc_interelectrode_spacing returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_interelectrode_spacing(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_interelectrode_spacing(),float))
 
   def test_calc_output_voltage(self):
     """
     calc_output_voltage returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_output_voltage(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_output_voltage(),float))
 
   def test_calc_contact_potential(self):
     """
     calc_contact_potential returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_contact_potential(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_contact_potential(),float))
 
   def test_calc_forward_current_density(self):
     """
     calc_forward_current_density returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_forward_current_density(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_forward_current_density(),float))
 
   def test_calc_back_current_density(self):
     """
     calc_back_current_density returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_back_current_density(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_back_current_density(),float))
 
   def test_calc_output_current_density(self):
     """
     calc_output_current_density returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_output_current_density(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_output_current_density(),float))
 
   def test_calc_output_power_density(self):
     """
     calc_output_power_density returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_output_power_density(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_output_power_density(),float))
 
   def test_calc_load_resistance(self):
     """
     calc_load_resistance returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_load_resistance(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_load_resistance(),float))
 
   def test_calc_carnot_efficiency(self):
     """
     calc_carnot_efficiency returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_carnot_efficiency(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_carnot_efficiency(),float))
 
   def test_calc_radiation_efficiency(self):
     """
     calc_radiation_efficiency returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_radiation_efficiency(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_radiation_efficiency(),float))
 
   def test_calc_electronic_efficiency(self):
     """
     calc_electronic_efficiency returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_electronic_efficiency(),float))
+    self.assertTrue(isinstance(self.TECBase_obj.calc_electronic_efficiency(),float))
 
   def test_calc_total_efficiency(self):
     """
     calc_total_efficiency returns numeric.
     """
-    self.assertTrue(isinstance(self.TEC_obj.calc_total_efficiency(),float))
-
-if __name__ == '__main__':
-  unittest.main()
+    self.assertTrue(isinstance(self.TECBase_obj.calc_total_efficiency(),float))
