2a90460c06194d5f51ebff173e7b638fcbe3d91e
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-04-20 20:52:32 -0400

    Added setup.py for pip, etc. Closes #18

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='tec',
+      version='0.3.0',
+      author='Joshua Ryan Smith',
+      author_email='joshua.r.smith@gmail.com',
+      packages=['tec','tec/models'],
+      url='https://github.com/jrsmith3/tec',
+      description='A python package for simulating vacuum thermionic energy conversion devices.',
+      install_requires=[
+      	'numpy',
+      	'scipy',
+      	'matplotlib',
+      	],
+	  test_suite='nose.collector',
+      tests_require=['nose'],
+      license='',
+      zip_safe=False)
