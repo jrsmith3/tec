48977f8fe0493ccabf8f7bf14c85adf05e46db25
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-06-18 11:13:00 -0400

    Implementation of physical property.

---
+++ electrode/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+=========================
+Base Library (:mod:`electrode`)
+=========================
+
+.. currentmodule:: electrode
+"""
+
+from physicalproperty import *
+import constants
