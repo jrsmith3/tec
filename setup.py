96cedc71991f9c6f0edc163638da46d8b46ac69d
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-07-20 18:23:45 -0400

    Bumped version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name = "electrode",
-      version = "1.0.0",
+      version = "1.1.0",
       author = "Joshua Ryan Smith",
       author_email = "joshua.r.smith@gmail.com",
       packages=['electrode'],
