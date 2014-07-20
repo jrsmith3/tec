e0b2377eaf9f7c0e646e73587d8599f4a1e735c4
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-07-20 16:27:09 -0400

    Installation file.

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name = "electrode",
+      version = "1.0.0",
+      author = "Joshua Ryan Smith",
+      author_email = "joshua.r.smith@gmail.com",
+      packages=['electrode'],
+      url = "https://github.com/jrsmith3/electrode",
+      description = "Simulator of thermoelectron emission from an electrode.",
+      classifiers = ["Programming Language :: Python",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Development Status :: 4 - Beta",
+                     "Intended Audience :: Science/Research",
+                     "Topic :: Scientific/Engineering :: Physics",
+                     "Natural Language :: English",],
+      install_requires=['numpy',
+                        'scipy',
+                        'astropy'],)
