45953b0f82ffff1f11a20a174f8e84e8f7cb0ef4
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-07-24 14:34:22 -0400

    Initial edits to comply with style guidelines.

--- test/test_SC_Electrode.py
+++ test/test_SC_Electrode.py
@@ -8,55 +8,71 @@ import copy
 
 # Values for Si taken from Streetman & Banerjee 9780130255389.
 input_params = {"temp": 300.,
+                "barrier": 1.0,
+                "richardson": 100.0,
+
                 "el_effective_mass": 9.84e-31,
                 "ho_effective_mass": 7.38e-31,
                 "accept_conc": 1e18,
                 "accept_ionization_energy": 45.,
-                "bandgap": 1.11,
+                "bandgap": 1.11,}
 
-                "barrier": 1.0,
-                "richardson": 100.0,}
 
 # Base classes
 # ============
-class ElectrodeAPITestBaseJustInputParams(unittest.TestCase):
+class TestBaseJustInputParams(unittest.TestCase):
     """
-    Base class for API tests.
+    Base class for tests.
 
-    This class defines a common setUp method that all the tests in this suite use.
+    This class defines a common setUp method that features an attribute which can be used to instantiate `SC_Electrode` objects.
     """
-
     def setUp(self):
         """
-        Set up a dictionary that can properly instantiate an Electrode object.
+        Set up a dictionary that can properly instantiate an `SC_Electrode` object.
         """
         self.input_params = copy.copy(input_params)
 
 
-class ElectrodeAPITestBaseWithElectrode(unittest.TestCase):
+class TestBaseWithElectrode(unittest.TestCase):
     """
-    Base class for API tests.
+    Base class for tests.
 
-    This class defines a common setUp method that all the tests in this suite use.
+    This class defines a common setUp method that features an attribute which is an `Electrode` object.
     """
-
     def setUp(self):
         """
-        Set up a dictionary that can properly instantiate an Electrode object.
+        Set up an `SC_Electrode` object.
         """
         self.El = SC_Electrode(copy.copy(input_params))
 
 
 # Test classes
 # ============
-class InstantiationInputIncomplete(ElectrodeAPITestBaseJustInputParams):
+class InstantiationInputNonDict(unittest.TestCase):
     """
-    Tests instantiating when input dict is missing required data.
+    Tests instantiation when non-dict data is used.
+    """
+    def test_no_input_arg(self):
+        """
+        SC_Electrode instantiation without input argument is invalid.
+        """
+        self.assertRaises(TypeError, SC_Electrode, None)
+
+    def test_non_dict_input_arg(self):
+        """
+        SC_Electrode instantiation with non-dict input argument is invalid.
+        """
+        self.assertRaises(TypeError, SC_Electrode, "this string is not a dict.")
+
+
+class InstantiationInputIncomplete(TestBaseJustInputParams):
+    """
+    Tests instantiation when non-dict data is used.
     """
     pass
 
 
-class InstantiationInputSuperfluousKeys(ElectrodeAPITestBaseJustInputParams):
+class InstantiationInputSuperfluousKeys(TestBaseJustInputParams):
     """
     SC_Electrode can be instantiated with dict with superfluous keys.
     """
@@ -70,14 +86,14 @@ class InstantiationInputSuperfluousKeys(ElectrodeAPITestBaseJustInputParams):
             self.fail("Superfluous key in input param dict caused failure of instantiation.")
 
 
-class InstantiationInputFieldsWrongType(ElectrodeAPITestBaseJustInputParams):
+class InstantiationInputFieldsWrongType(TestBaseJustInputParams):
     """
     Tests instantiating when input dict has non-numeric data items.
     """
     pass
 
 
-class InstantiationInputOutsideConstraints(ElectrodeAPITestBaseJustInputParams):
+class InstantiationInputOutsideConstraints(TestBaseJustInputParams):
     """
     Tests instantiating when input dict values are outside their constraints.
 
@@ -87,14 +103,14 @@ class InstantiationInputOutsideConstraints(ElectrodeAPITestBaseJustInputParams):
     pass
 
 
-class SetDataWrongType(ElectrodeAPITestBaseWithElectrode):
+class SetDataWrongType(TestBaseWithElectrode):
     """
     Tests setting attributes when input data is non-numeric.
     """
     pass
 
 
-class SetDataOutsideConstraints(ElectrodeAPITestBaseWithElectrode):
+class SetDataOutsideConstraints(TestBaseWithElectrode):
     """
     Tests setting attributes when input values are outside their constraints.
 
@@ -104,22 +120,15 @@ class SetDataOutsideConstraints(ElectrodeAPITestBaseWithElectrode):
     pass
 
 
-class CalculatorsReturnTypeAndUnits(ElectrodeAPITestBaseWithElectrode):
+class CalculatorsReturnType(TestBaseWithElectrode):
     """
-    Tests output types and units (where applicable) of the Electrode calculator methods.
+    Tests output types of the calculator methods.
     """
-
     def test_calc_cond_band_effective_dos_type(self):
         """
         calc_cond_band_effective_dos should return an astropy.units.Quantity.
         """
         self.assertIsInstance(self.El.calc_cond_band_effective_dos(), Quantity)
-
-    def test_calc_cond_band_effective_dos_unit(self):
-        """
-        calc_cond_band_effective_dos should return a value with unit 1/cm3.
-        """
-        self.assertEqual(self.El.calc_cond_band_effective_dos().unit, Unit("1/cm3"))
 
     def test_calc_val_band_effective_dos_type(self):
         """
@@ -127,23 +136,11 @@ class CalculatorsReturnTypeAndUnits(ElectrodeAPITestBaseWithElectrode):
         """
         self.assertIsInstance(self.El.calc_val_band_effective_dos(), Quantity)
 
-    def test_calc_val_band_effective_dos_unit(self):
-        """
-        calc_val_band_effective_dos should return a value with unit 1/cm3.
-        """
-        self.assertEqual(self.El.calc_val_band_effective_dos().unit, Unit("1/cm3"))
-
     def test_calc_el_carrier_conc_type(self):
         """
         calc_el_carrier_conc should return an astropy.units.Quantity.
         """
         self.assertIsInstance(self.El.calc_el_carrier_conc(), Quantity)
-
-    def test_calc_el_carrier_conc_unit(self):
-        """
-        calc_el_carrier_conc should return a value with unit 1/cm3.
-        """
-        self.assertEqual(self.El.calc_el_carrier_conc().unit, Unit("1/cm3"))
 
     def test_calc_ho_carrier_conc_type(self):
         """
@@ -151,17 +148,40 @@ class CalculatorsReturnTypeAndUnits(ElectrodeAPITestBaseWithElectrode):
         """
         self.assertIsInstance(self.El.calc_ho_carrier_conc(), Quantity)
 
-    def test_calc_ho_carrier_conc_unit(self):
-        """
-        calc_ho_carrier_conc should return a value with unit 1/cm3.
-        """
-        self.assertEqual(self.El.calc_ho_carrier_conc().unit, Unit("1/cm3"))
-
     def test_calc_fermi_energy_type(self):
         """
         calc_fermi_energy should return an astropy.units.Quantity.
         """
         self.assertIsInstance(self.El.calc_fermi_energy(), Quantity)
+
+
+class CalculatorsReturnUnits(TestBaseWithElectrode):
+    """
+    Tests output units, where applicable, of the calculator methods.
+    """
+    def test_calc_cond_band_effective_dos_unit(self):
+        """
+        calc_cond_band_effective_dos should return a value with unit 1/cm3.
+        """
+        self.assertEqual(self.El.calc_cond_band_effective_dos().unit, Unit("1/cm3"))
+
+    def test_calc_val_band_effective_dos_unit(self):
+        """
+        calc_val_band_effective_dos should return a value with unit 1/cm3.
+        """
+        self.assertEqual(self.El.calc_val_band_effective_dos().unit, Unit("1/cm3"))
+
+    def test_calc_el_carrier_conc_unit(self):
+        """
+        calc_el_carrier_conc should return a value with unit 1/cm3.
+        """
+        self.assertEqual(self.El.calc_el_carrier_conc().unit, Unit("1/cm3"))
+
+    def test_calc_ho_carrier_conc_unit(self):
+        """
+        calc_ho_carrier_conc should return a value with unit 1/cm3.
+        """
+        self.assertEqual(self.El.calc_ho_carrier_conc().unit, Unit("1/cm3"))
 
     def test_calc_fermi_energy_unit(self):
         """
@@ -169,11 +189,9 @@ class CalculatorsReturnTypeAndUnits(ElectrodeAPITestBaseWithElectrode):
         """
         self.assertEqual(self.El.calc_fermi_energy().unit, Unit("eV"))
 
-if __name__ == "__main__":
-    sc = SC_Electrode(input_params)
-    print "Conduction band dos", str(sc.calc_cond_band_effective_dos())
-    print "Valence band dos", str(sc.calc_val_band_effective_dos())
 
-    print "Fermi energy", str(sc.calc_fermi_energy())
-    print "Electron concentration", str(sc.calc_el_carrier_conc())
-    print "Hole concentration", str(sc.calc_ho_carrier_conc())
+class CalculatorsReturnValues(TestBaseWithElectrode):
+    """
+    Tests values of calculator methods against known values.
+    """
+    pass
