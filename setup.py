6000037465bf6974fff763a582620eb117f2ce42
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-09 18:37:32 -0500

    Clean up formatting

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
+# -*- coding: utf-8 -*-
 from distutils.core import setup
 import tec
 
-setup(name='tec',
-      version=tec.__version__,
-      author='Joshua Ryan Smith',
-      author_email='joshua.r.smith@gmail.com',
-      packages=['tec','tec/models'],
-      url='https://github.com/jrsmith3/tec',
-      description='A python package for simulating vacuum thermionic energy conversion devices.',
-      install_requires=[
-      	'numpy',
-      	'scipy',
-      	'matplotlib',
-      	],
-	  test_suite='nose.collector',
-      tests_require=['nose'],
-      license='',
-      zip_safe=False)
+setup(name = "tec",
+      version = tec.__version__,
+      author = "Joshua Ryan Smith",
+      author_email = "joshua.r.smith@gmail.com",
+      packages = ["tec","tec/models"],
+      url = "https://github.com/jrsmith3/tec",
+      description = "Utils for simulating vacuum thermionic energy conversion devices",
+      classifiers = ["Programming Language :: Python",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Development Status :: 4 - Beta",
+                     "Intended Audience :: Science/Research",
+                     "Topic :: Scientific/Engineering :: Physics",
+                     "Natural Language :: English",],
+      install_requires = ["numpy",
+                    	  "scipy",
+                  	  "matplotlib",],
+      license = "MIT",)
