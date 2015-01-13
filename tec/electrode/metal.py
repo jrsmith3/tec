8c9b63ce8e637e760cdfab724c218192c93a1da8
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-01-13 15:35:35 -0500

    Fix some pep8 issues

--- tec/electrode/metal.py
+++ tec/electrode/metal.py
@@ -5,35 +5,35 @@ from astropy import units
 from astropy import constants
 from physicalproperty import PhysicalProperty, find_PhysicalProperty
 
+
 class Metal(object):
     """
     Metal thermoelectron electrode
 
     A `Metal` electrode is instantiated with a dict having keys identical to the class's public data attributes. Each key's value must satisfy the constraints noted with the corresponding public data attribute. Dictionary values can be some kind of numeric type or of type `astropy.units.Quantity` so long as the units are compatible with what's listed.
     """
-    temp = PhysicalProperty(unit = "K", lo_bnd = 0)
+    temp = PhysicalProperty(unit="K", lo_bnd=0)
     """
     Temperature
 
     :symbol: :math:`T`
     """
 
-    barrier = PhysicalProperty(unit = "eV", lo_bnd = 0)
+    barrier = PhysicalProperty(unit="eV", lo_bnd=0)
     """
-    Emission barrier (a.k.a. work function) 
+    Emission barrier (a.k.a. work function)
 
-    The barrier is the difference between the vacuum energy of the surface and the Fermi energy. 
+    The barrier is the difference between the vacuum energy of the surface and the Fermi energy.
 
     :symbol: :math:`\phi`
     """
 
-    richardson = PhysicalProperty(unit = "A/(cm2 K2)", lo_bnd = 0)
+    richardson = PhysicalProperty(unit="A/(cm2 K2)", lo_bnd=0)
     """
     Richardson constant
 
     :symbol: :math:`A`
     """
-
 
     def __init__(self, params):
         for attr in find_PhysicalProperty(self):
@@ -67,10 +67,10 @@ class Metal(object):
         :symbol: :math:`J_{RD}`
         """
         if self.temp.value == 0:
-          current_density = units.Quantity(0, "A/cm2")
+            current_density = units.Quantity(0, "A/cm2")
         else:
             exponent = (self.barrier / (constants.k_B * self.temp)).decompose()
             coefficient = self.richardson * self.temp**2
             current_density = coefficient * np.exp(-exponent)
-        
+
         return current_density.to("A/cm2")
