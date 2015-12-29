1c4df5412382721b82af1588983e369020e820b8
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-12-29 16:37:12 -0500

    Fix typos in tests

--- test/test_Langmuir.py
+++ test/test_Langmuir.py
@@ -60,14 +60,14 @@ class MethodsInput(Base):
         critical_point_target_function should raise ValueError if input is greater than the upper bound
         """
         current_density = 2 * self.t.emitter.thermoelectron_current_density()
-        self.assertRaises(ValueError, self.t.critical_point_target_function(current_density))
+        self.assertRaises(ValueError, self.t.critical_point_target_function, current_density)
 
     def test_critical_point_target_function_below_bound(self):
         """
         critical_point_target_function should raise ValueError if input is greater than the upper bound
         """
         current_density = -1.
-        self.assertRaises(ValueError, self.t.critical_point_target_function(current_density))
+        self.assertRaises(ValueError, self.t.critical_point_target_function, current_density)
 
 
 class MethodsReturnType(Base):
