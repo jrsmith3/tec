7053ff2635f5784f00456b581da8c341f915aa0b
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2022-12-19 08:42:04 -0500

    Add test cases

--- test/test_TEC.py
+++ test/test_TEC.py
@@ -33,6 +33,34 @@ def test_tec_constructor_happy_path(valid_ideal_model):
                 "back_current_density",
                 astropy.units.Quantity(3.92658733e-07, "A/cm2"),
             ),
+            (
+                "output_current_density",
+                astropy.units.Quantity(7.71157739e-07, "A/cm2"),
+            ),
+            (
+                "output_power_density",
+                astropy.units.Quantity(3.8557887e-06, "W/cm2"),
+            ),
+            (
+                "carnot_efficiency",
+                astropy.units.Quantity(0.85, astropy.units.dimensionless_unscaled),
+            ),
+            (
+                "efficiency",
+                astropy.units.Quantity(4.25207974e-08, astropy.units.dimensionless_unscaled),
+            ),
+            (
+                "heat_supply_rate",
+                astropy.units.Quantity(90.68006553, astropy.units.W),
+            ),
+            (
+                "electron_cooling_rate",
+                astropy.units.Quantity(4.85357264e-06, astropy.units.W),
+            ),
+            (
+                "thermal_radiation_rate",
+                astropy.units.Quantity(90.68006067, astropy.units.W),
+            ),
         ]
     )
 def test_methods_regression(valid_ideal_model, method_under_test, expected_output):
