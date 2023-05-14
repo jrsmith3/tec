44f1fc46f01208d1b4fdc9bbd7e4efd6349ecbc1
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2023-05-14 15:52:30 -0400

    Add test

--- test/test_Langmuir.py
+++ test/test_Langmuir.py
@@ -89,10 +89,16 @@ class TestLangmuirMethodsConsistency():
                 norm_l = valid_langmuir_model.normalization_length(current_density)
 
 
+    @pytest.mark.parametrize("current_density", [
+            0.5,
+            astropy.units.Quantity(0.5, "A/cm2"),
+            ]
+        )
+    def test_normalization_length_current_density_gt_0_does_not_raise(self, valid_langmuir_model, current_density):
+        with does_not_raise():
+                norm_l = valid_langmuir_model.normalization_length(current_density)
 
 
-def test_normalization_length_current_density_gt_0(self, valid_langmuir_model):
-    pass
 
 
 @pytest.fixture
